ea47e406bd4ea9e8c28d68f85160b6d8d2fa6b72
Dan Sinclair <dan.sinclair@poqcommerce.com>
2021-05-13 16:36:18 +0100

    Setup for PyPi

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-     name='duco',  
-     version='0.1',
-     scripts=['duco'] ,
-     author="Dan Sinclair",
-     author_email="dansinclair@me.com",
-     description="A python package for the Duino Coin REST API",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/dansinclair25/duco-py",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
+    name='duco',  
+    version='0.1.0',
+    author="Dan Sinclair",
+    author_email="dansinclair@me.com",
+    description="A python package for the Duino Coin REST API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dansinclair25/duco-py",
+    packages=['duco'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
  )
